d48c749006ba3412f10560fc8a4a19c58eac592a
kay-sb <kianoushsabouri@gmail.com>
2024-10-23 15:21:50 +0330

    Initialize project and set up basic dependencies - Installed Tailwind CSS for styling. - Installed @heroicons/react for icons. - Installed framer-motion for animations. - Installed react-dom - Installed react-icons - Installed react-router-dom - Installed react-syntax-highlighter for code highlighting.

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+const App: React.FC = () => {
+  return (
+    <Router>
+
+    </Router>
+  );
+};
+
+export default App;
