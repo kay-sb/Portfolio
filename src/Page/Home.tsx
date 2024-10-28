5531b548d08fc504151812c45cf44fc8c30c1c23
kay-sb <kianoushsabouri@gmail.com>
2024-10-28 11:52:17 +0330

    refactor: remove portfolio component and transfer content to home page - Deleted the portfolio component and moved its content to the home page for better organization and structure.

--- src/Page/Home.tsx
+++ src/Page/Home.tsx
@@ -1,9 +1,27 @@
-import React from "react";
-import Portfolio from "../components/Portfolio";
+import React, { useEffect } from "react";
+import Profile from "../components/ProfileCart";
+import { useTheme } from "../components/ThemeContext"; // Importing ThemeContext for managing dark/light mode
+import About from "../components/About";
+import ProjectList from "../components/ProjectList";
 
 const HomePage: React.FC = () => {
+  const { darkMode } = useTheme();
+
+  useEffect(() => {
+    document.body.classList.toggle("dark-mode", darkMode); // Apply dark mode class to body
+    document.body.classList.toggle("light-mode", !darkMode); // Apply light mode class to body
+  }, [darkMode]);
+
   return (
-    <Portfolio />
+    <div className="min-h-screen flex flex-col items-center ">
+    <div className="w-full max-w-6xl flex flex-col md:flex-row items-center justify-center mt-20 ">
+      <Profile/>
+      <div className="flex-1 text-center md:text-left mt-10 md:mt-0">
+        <About />
+        <ProjectList/>
+      </div>
+    </div>
+  </div>
   );
 };
 
