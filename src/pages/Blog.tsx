9489dbf13665bd4e9cc48a85787ebfae179b6e61
kay-sb <kianoushsabouri@gmail.com>
2024-10-30 20:16:35 +0330

    Fix component styles

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -51,65 +51,67 @@ const BlogPage: React.FC = () => {
         <div className="hidden md:flex">
           <Profile />
         </div>
-        <div className="mt-5 mx-auto max-w-[80%]">
-          <div
-            className={`p-4 rounded-lg mb-5 ${
-              darkMode ? "bg-text-title2-light" : "bg-text-title2-dark"
-            }`}
-          >
-            <h1
-              className={`text-xl font-bold mb-4 ${
+        <div className="mt-5 mx-auto w-full">
+          <div className="w-full max-w-[80%] mx-auto md:mx-0">
+            <div
+              className={`p-4 rounded-lg mb-5 ${
+                darkMode ? "bg-text-title2-light" : "bg-text-title2-dark"
+              }`}
+            >
+              <h1
+                className={`text-xl font-bold mb-4 ${
+                  darkMode ? "text-text-title-light" : "text-text-title-dark"
+                }`}
+              >
+                {blog.title}
+              </h1>
+              <p className="text-sm mb-4 text-gray-500">
+                {blog.date} • {blog.readTime}
+              </p>
+            </div>
+            <p
+              className={`mb-8 leading-7 ${
                 darkMode ? "text-text-title-light" : "text-text-title-dark"
               }`}
             >
-              {blog.title}
-            </h1>
-            <p className="text-sm mb-4 text-gray-500">
-              {blog.date} • {blog.readTime}
+              {blog.introduction}
             </p>
-          </div>
-          <p
-            className={`mb-8 leading-7 ${
-              darkMode ? "text-text-title-light" : "text-text-title-dark"
-            }`}
-          >
-            {blog.introduction}
-          </p>
 
-          {blog.sections?.map((section, index) => (
-            <div key={index} className="mb-8">
-              <h2
-                className={`text-2xl font-semibold mb-4 ${
-                  darkMode ? "text-text-title-light" : "text-text-title-dark"
-                }`}
-              >
-                {section.heading}
-              </h2>
-              <p
-                className={`mb-4 ${
-                  darkMode ? "text-text-title-light" : "text-text-title-dark"
-                }`}
-              >
-                {section.description}
-              </p>
-              {section.list && (
-                <ul className="list-disc list-inside">
-                  {section.list.map((item, i) => (
-                    <li
-                      key={i}
-                      className={`mb-2 ${
-                        darkMode
-                          ? "text-text-title-light"
-                          : "text-text-title-dark"
-                      }`}
-                    >
-                      <strong>{item.name}:</strong> {item.description}
-                    </li>
-                  ))}
-                </ul>
-              )}
-            </div>
-          ))}
+            {blog.sections?.map((section, index) => (
+              <div key={index} className="mb-8">
+                <h2
+                  className={`text-2xl font-semibold mb-4 ${
+                    darkMode ? "text-text-title-light" : "text-text-title-dark"
+                  }`}
+                >
+                  {section.heading}
+                </h2>
+                <p
+                  className={`mb-4 ${
+                    darkMode ? "text-text-title-light" : "text-text-title-dark"
+                  }`}
+                >
+                  {section.description}
+                </p>
+                {section.list && (
+                  <ul className="list-disc list-inside">
+                    {section.list.map((item, i) => (
+                      <li
+                        key={i}
+                        className={`mb-2 ${
+                          darkMode
+                            ? "text-text-title-light"
+                            : "text-text-title-dark"
+                        }`}
+                      >
+                        <strong>{item.name}:</strong> {item.description}
+                      </li>
+                    ))}
+                  </ul>
+                )}
+              </div>
+            ))}
+          </div>
           <Form />
           <div className="flex justify-center items-center md:hidden">
             <Profile />
